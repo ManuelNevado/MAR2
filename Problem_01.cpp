860616ab1513448cbbfb7a4eb39548f7678df3c2
Manuel <manuneva@ucm.es>
2023-01-26 09:53:30 +0100

    Segmentation fault en ingeniero

--- Problem_01.cpp
+++ Problem_01.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Matriz.h"
+#include "EnterosInf.h"
 #include <algorithm>
 using namespace std;
 
@@ -8,11 +9,15 @@ struct cuerda{
     int precio;
 };
 
-
+void print_vint(vector<int> v,int n){
+    for(int i=0;i<n;i++)
+        cout<<v[i]<<' ';
+    cout<<'\n';
+}
 bool posible(vector<cuerda> cuerdas,int n, int l);
 int matematico(vector<cuerda> cuerdas, int n, int l);
 int economista(vector<cuerda> cuerdas,int n, int l);
-int ingeniero(vector<cuerda> cuerdas, int n, int l);
+EntInf ingeniero(vector<cuerda> cuerdas, int n, int l);
 
 
 int main(){
@@ -29,7 +34,7 @@ int main(){
         }
 
         if(posible(cuerdas,n,l))
-        cout<<"SI "<<matematico(cuerdas,n,l)<<'\n';//<<' '<<ingeniero(cuerdas,n,l)<<' '<<economista(cuerdas,n,l)<<endl;
+        cout<<"SI "<<matematico(cuerdas,n,l)<<' '<<ingeniero(cuerdas,n,l)<<endl;//<<' '<<economista(cuerdas,n,l)<<endl;
         else
             cout<<"NO\n";
     }
@@ -74,29 +79,36 @@ bool posible(vector<cuerda> cuerdas, int n, int l){
 }
 
 int matematico(vector<cuerda> cuerdas, int n, int l){
-    Matriz<int> matriz(n+1,l+1);
+    vector<int> v(l+1,0);
+    
+    v[0] = 1;//Solo se puede hacer una cuerda de longitud 0 de 1 manera, con 0 cuerdas
 
-    //Casos base
-    for(int i=1;i<=l;i++)
-        matriz[0][i]=0;
-
-    for(int i=0;i<=n;i++){
-        matriz[i][0]=0;
-    }
-
-    matriz[0][0] = 1;
-
-    //Casos recursivos
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=l;j++){
-            if(cuerdas[i-1].longitud > j){
-                matriz[i][j] = matriz[i-1][j];
-            }else{
-                matriz[i][j] = matriz[i-1][j]+matriz[i][j-cuerdas[i-1].longitud];
-            }
+    for(int i=0;i<n;i++){
+        for(int j=l;j>=0;j--){
+            int i_prev = j - cuerdas[i].longitud;
+            v[j] += v[i_prev];
         }
     }
 
-    cout<<matriz;
-    return matriz[n][l];
+    //print_vint(v,l+1);
+    return v[l];
+}
+
+
+EntInf ingeniero(vector<cuerda> cuerdas, int n, int l){
+
+    vector<EntInf> v(l+1,Infinito);
+    
+
+    //Caso recursivo
+    for(int i=0;i<n;i++){
+        cout<<"i: "<<i<<"\nj: "<<l<<"\nhasta: "<<cuerdas[i].longitud<<endl;
+        for(int j =l;l>=cuerdas[i].longitud;j--){
+            int prev_index = j - cuerdas[i].longitud;
+            cout<<"prev_index: "<<prev_index<<endl;
+            v[j] = min(v[j],v[prev_index]+1);
+        }
+    }
+    //cout<<matriz;
+    return v[l];
 }
