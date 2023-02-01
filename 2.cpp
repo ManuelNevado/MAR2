db013c1d3547316baa8f83d55e590502d5a64a3f
Manuel Nevado Fabian <manuneva@ucm.es>
2023-02-01 11:17:12 +0100

    dianas working

--- 2.cpp
+++ 2.cpp
@@ -14,7 +14,7 @@ void print_v(std::vector<T> sol){
 
 EntInf max(EntInf const& a, EntInf const& b);
 void sol(int puntos, int n, std::vector<int> v);
-void reconstruir_sol(Matriz<EntInf> matriz, int puntos, int n, std::vector<int> v);
+void reconstruir_sol(std::vector<EntInf> sol, int puntos, int n, std::vector<int> v);
 
 int main(){
     int puntos, n;
@@ -30,31 +30,23 @@ int main(){
 
 void sol(int puntos, int n, std::vector<int> v){
     // Es igual que las monedas
-    Matriz<EntInf> matriz(n+1, puntos+1, Infinito);
 
-    //Casos base
-    for(int i=0;i<=n;i++)
-        matriz[i][0] = 0;
+    std::vector<EntInf> sol(puntos+1,Infinito);
 
-    for (int i=1;i<=puntos;i++)
-        matriz[0][i] = Infinito;
-
-
+    //Caso base
+    sol[0] = 0;//El minimo numero de dardos para lograr la puntuacion 0 es 0
+    
     //Caso recursivo
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=puntos;j++){
-            if(v[i-1] > j)
-                matriz[i][j] = matriz[i-1][j];
-            else
-                matriz[i][j] = std::min(matriz[i-1][j],matriz[i][j-v[i]]+1);
+    for(int i = 1;i<=n;i++){
+        for(int j = v[i-1]; j<=puntos;j++){
+            sol[j] = std::min(sol[j], sol[j-v[i-1]]+1);
         }
     }
 
-    std::cout<< matriz;
-    EntInf sol = matriz[n][puntos];
-    if(sol != Infinito){
-        std::cout<<sol<<":";
-        reconstruir_sol(matriz,n,puntos,v);
+    
+    if(sol[puntos] != Infinito){
+        std::cout<<sol[puntos]<<":";
+        reconstruir_sol(sol,n,puntos,v);
     }else
         std::cout<<"Imposible\n";
     
@@ -67,30 +59,20 @@ EntInf max_elem(EntInf const& a, EntInf const& b){
         return std::max(a,b);
 }
 
-void reconstruir_sol(Matriz<EntInf> matriz, int n, int puntos, std::vector<int> v){
+void reconstruir_sol(std::vector<EntInf> sol, int n, int puntos, std::vector<int> v){
+    
 
-    std::vector<EntInf> sol;
-    int i=n, j=puntos;
+    std::vector<EntInf> puntuaciones;
+    int j=puntos, i=n-1;
 
-    while(j>0 && i>0){
-        std::cout<<"Vector: \n";
-        print_v(sol);
-        std::cout<<"Current i,j: "<<i<<", "<<j<<std::endl;
-        if(v[i-1] <= j){
-            EntInf a = matriz[i-1][j];
-            EntInf b = matriz[i][j-v[i-1]];
-            EntInf max = max_elem(a,b);
-            if(max == a)
-                i--;
-            else{
-                sol.push_back(v[i-1]);
-                j -= v[i-1];
-            }
+    while(j>0){
+        if(v[i] <= j && sol[j] > sol[j - v[i]]){
+            puntuaciones.push_back(v[i]);
+            j-=v[i];
         }else
             i--;
     }
-    
-    print_v(sol);
 
+    print_v(puntuaciones);
 }
 
